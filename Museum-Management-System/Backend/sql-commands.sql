50efbaad548b6a268f6fc0a8a38e11386a9227ac
Cipher <cipher@rohannk.com>
2024-02-03 18:44:43 +0530

    changed order

--- Museum-Management-System/Backend/sql-commands.sql
+++ Museum-Management-System/Backend/sql-commands.sql
@@ -12,6 +12,12 @@ create table CATEGORY(
     description text
 );
 
+create table CURATOR(
+    curator_id int primary key,
+    name varchar(255),
+    email_id varchar(255),
+);
+
 create table ARTIFACT(
     artifact_id int primary key,
     name varchar(255),
@@ -24,12 +30,6 @@ create table ARTIFACT(
     foreign key (artist_id) references ARTIST(artist_id) on delete set null,
     foreign key (category_id) references CATEGORY(category_id) on delete set null,
     foreign key (curator_id) references CURATOR(curator_id) on delete set null
-);
-
-create table CURATOR(
-    curator_id int primary key,
-    name varchar(255),
-    email_id varchar(255),
 );
 
 create table EXHIBITION(
