fa17ec218a406b22b8636a18f51a039fbeccfe7f
vis1003 <vis21cs@cmrit.ac.in>
2024-02-09 23:34:54 +0530

    created navbar

--- Museum-Management-System/Angular/Museum-app/src/app/header/header.component.ts
+++ Museum-Management-System/Angular/Museum-app/src/app/header/header.component.ts
@@ -3,66 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   template: `
-  <nav class="navbar" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-    </a>
-
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
-
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-start">
+  <div class="navbar is-dark">
+    <div class="navbar-brand">
       <a class="navbar-item">
-        Home
+        <img src="">
       </a>
-
-      <a class="navbar-item">
-        Documentation
-      </a>
-
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          More
-        </a>
-
-        <div class="navbar-dropdown">
-          <a class="navbar-item">
-            About
-          </a>
-          <a class="navbar-item">
-            Jobs
-          </a>
-          <a class="navbar-item">
-            Contact
-          </a>
-          <hr class="navbar-divider">
-          <a class="navbar-item">
-            Report an issue
-          </a>
-        </div>
-      </div>
     </div>
 
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <a class="button is-primary">
-            <strong>Sign up</strong>
-          </a>
-          <a class="button is-light">
-            Log in
-          </a>
-        </div>
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item" routerLink="/">Home</a>
+        <a class="navbar-item" routerLink="/artifacts">Artifacts</a>
+        <a class="navbar-item" routerLink="/artists">Artists</a>
+        <a class="navbar-item" routerLink="/curators">Curators</a>
+        <a class="navbar-item" routerLink="/categories">Categories</a>
+        <a class="navbar-item" routerLink="/exhibitions">Exhibitions</a>
       </div>
     </div>
   </div>
-</nav>
   `,
   styles: ``
 })
