722b11c5b97eb0e3c754ddd477d749b2b4d22060
vis1003 <vis21cs@cmrit.ac.in>
2024-02-14 11:48:23 +0530

    created separate html files for components

--- Museum-Management-System/Angular/Museum-app/src/app/artifacts/artifacts.component.ts
+++ Museum-Management-System/Angular/Museum-app/src/app/artifacts/artifacts.component.ts
@@ -3,36 +3,7 @@ import { AllDataService } from '../services/all-data.service';
 
 @Component({
   selector: 'app-artifacts',
-  template: `
-    <p class="title is-2" style="padding: 1%;">Artifacts</p>
-
-    <table class="table is-hoverable is-bordered" style="margin: 1%;">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Artifact Name</th>
-          <th>Description</th>
-          <th>Acquisition Date</th>
-          <th>Condition</th>
-          <th>Artist</th>
-          <th>Category</th>
-          <th>Curator</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr *ngFor="let item of artifactData['data']">
-          <td>{{item.artifact_id}}</td>
-          <td>{{item.artifact_name}}</td>
-          <td>{{item.artifact_description}}</td>
-          <td>{{item.acquisition_date}}</td>
-          <td>{{item.artifact_condition}}</td>
-          <td>{{item.artist_name}}</td>
-          <td>{{item.category_name}}</td>
-          <td>{{item.curator_name}}</td>
-        </tr>
-      </tbody>
-    </table>
-  `,
+  templateUrl: './artifacts.component.html',
   styles: ``
 })
 export class ArtifactsComponent implements OnInit{
--- Museum-Management-System/Angular/Museum-app/src/app/curators/curators.component.ts
+++ Museum-Management-System/Angular/Museum-app/src/app/curators/curators.component.ts
@@ -3,26 +3,7 @@ import { AllDataService } from '../services/all-data.service';
 
 @Component({
   selector: 'app-curators',
-  template: `
-  <p class="title is-2" style="padding: 1%;">Curators</p>
-
-  <table class="table is-hoverable is-bordered" style="margin: 1%;">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Curator Name</th>
-          <th>Email</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr *ngFor="let item of curatorData['data']">
-          <td>{{item.curator_id}}</td>
-          <td>{{item.name}}</td>
-          <td>{{item.email_id}}</td>
-        </tr>
-      </tbody>
-    </table>
-  `,
+  templateUrl: './curators.component.html',
   styles: ``
 })
 export class CuratorsComponent implements OnInit{
