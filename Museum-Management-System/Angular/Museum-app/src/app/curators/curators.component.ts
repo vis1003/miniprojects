17126f2220efed871d42b80aaf9e3c8f23956278
vis1003 <vis21cs@cmrit.ac.in>
2024-02-24 18:19:30 +0530

    edit and delete for curator done

--- Museum-Management-System/Angular/Museum-app/src/app/curators/curators.component.ts
+++ Museum-Management-System/Angular/Museum-app/src/app/curators/curators.component.ts
@@ -21,6 +21,14 @@ export class CuratorsComponent implements OnInit{
     curator_email: new FormControl('')
   });
 
+  editCurator = new FormGroup({
+    curator_id: new FormControl(''),
+    curator_name: new FormControl(''),
+    curator_email: new FormControl('')
+  });
+
+  editCuratorId: any = null;
+
   curatorData: any=[];
 
   ngOnInit(): void {
@@ -62,4 +70,60 @@ SaveData() {
   });
   window.location.reload();
 }  
+
+deleteData(curator_id : any){
+  console.log(curator_id);
+  var check = confirm("Are you sure you want to delete Curator "+curator_id+"?")
+  if(check){
+    this.curator.deleteData(curator_id,'curator').subscribe((result)=>{
+      console.log(result);
+    });
+    window.location.reload();
+  }
+}
+
+editDataBtn(curator_id : any){
+  console.log(curator_id);
+  this.editCuratorId = curator_id;
+  this.curator.getDataByID('curator',curator_id).subscribe((result:any)=>{
+
+    const formData = this.editCurator.value;
+    console.log(result['data'])
+    
+    this.editCurator = new FormGroup({
+      curator_id: new FormControl(result['data'][0].curator_id),
+      curator_name: new FormControl(result['data'][0].name),
+      curator_email: new FormControl(result['data'][0].email_id)
+    });
+  });
+
+  const modal = document.querySelector('#modal-edit');
+  const modalBg = document.querySelector('#edit_background');
+  if (modal) {
+    modal.classList.add('is-active');
+    modalBg?.addEventListener('click', () => {
+      modal.classList.remove('is-active');
+    })
+  }
+}
+
+editData() {
+  let curator_id = this.editCuratorId;
+  console.log(this.editCurator.value);
+  
+  const formData = this.editCurator.value;
+  formData.curator_id = this.curatorData['data'].length+1
+  if (
+    formData.curator_id === "" || 
+    formData.curator_name === "" || 
+    formData.curator_email === ""
+  ) {
+    alert("Please Enter All Data!");
+  }
+  console.log(formData);
+  this.curator.updateData(formData,'curator',curator_id).subscribe((result)=>{
+    console.log(result);
+  });
+  window.location.reload();
+}
 }
